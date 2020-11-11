9fd458cd88e9a8978aa857c981d261fe51dfedfd
rabbit <rabbit.zheng@qiffa.com>
2020-11-11 19:28:20 +0800

    Update local language

--- SwitchGamepad_en_US.ts
+++ SwitchGamepad_en_US.ts
@@ -1,3 +1,456 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="SwitchGamepad_en_US"></TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>Gamepad</name>
+    <message>
+        <location filename="gamepad.ui" line="14"/>
+        <location filename="gamepad.ui" line="146"/>
+        <source>Gamepad</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="28"/>
+        <source>General</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="34"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="40"/>
+        <location filename="gamepad.cpp" line="163"/>
+        <source>Open</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="56"/>
+        <location filename="gamepad.ui" line="60"/>
+        <source>1200</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="65"/>
+        <source>2400</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="70"/>
+        <source>4800</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="75"/>
+        <source>9600</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="80"/>
+        <source>19200</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="85"/>
+        <source>38400</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="90"/>
+        <source>57600</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="95"/>
+        <source>115200</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="106"/>
+        <location filename="gamepad.ui" line="965"/>
+        <location filename="gamepad.ui" line="987"/>
+        <source>Refresh</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="167"/>
+        <source>LX (C)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="189"/>
+        <source>RY (Down)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="211"/>
+        <source>B (K)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="233"/>
+        <source>R  (U)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="255"/>
+        <source>Y (J)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="277"/>
+        <source>LC (F)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="299"/>
+        <source>RC (H)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="321"/>
+        <source>Up (W)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="343"/>
+        <source>LY (Z)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="365"/>
+        <source>+ (Y)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="387"/>
+        <source>Home (N)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="409"/>
+        <source>LY (X)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="431"/>
+        <source>Down (S)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="453"/>
+        <source>Right (D)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="475"/>
+        <source>A (L)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="497"/>
+        <source>RX (Right)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="519"/>
+        <source>RY (Up)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="541"/>
+        <source>LX (V)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="563"/>
+        <source>RX (Up)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="585"/>
+        <source>L (E)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="595"/>
+        <source>Keyboard Contrtol</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="617"/>
+        <source>ZL (Q)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="639"/>
+        <source>X (I)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="661"/>
+        <source>Left (A)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="683"/>
+        <source>Screenshot (B)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="705"/>
+        <source>ZR (O)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="727"/>
+        <source>- (R)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="735"/>
+        <source>Script</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="757"/>
+        <location filename="gamepad.ui" line="914"/>
+        <source>Add</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="765"/>
+        <source>ZL</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="770"/>
+        <source>L</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="775"/>
+        <source>ZR</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="780"/>
+        <source>R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="785"/>
+        <location filename="gamepad.ui" line="958"/>
+        <source>-</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="790"/>
+        <location filename="gamepad.ui" line="951"/>
+        <source>+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="795"/>
+        <source>LY MIN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="800"/>
+        <source>LY MAX</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="805"/>
+        <source>LX MIN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="810"/>
+        <source>LX MAX</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="815"/>
+        <source>LC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="820"/>
+        <source>A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="825"/>
+        <source>B</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="830"/>
+        <source>X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="835"/>
+        <source>Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="840"/>
+        <source>Up</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="845"/>
+        <source>Down</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="850"/>
+        <source>Left</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="855"/>
+        <source>Right</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="860"/>
+        <source>RY MIN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="865"/>
+        <source>RY MAX</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="870"/>
+        <source>RX MIN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="875"/>
+        <source>RX MAX</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="880"/>
+        <source>RC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="885"/>
+        <source>Screenshot</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="890"/>
+        <source>Home</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="997"/>
+        <source>Save</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="1000"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.ui" line="1010"/>
+        <location filename="gamepad.cpp" line="25"/>
+        <location filename="gamepad.cpp" line="351"/>
+        <source>Run</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="25"/>
+        <location filename="gamepad.cpp" line="351"/>
+        <source>Stop</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="31"/>
+        <location filename="gamepad.cpp" line="155"/>
+        <location filename="gamepad.cpp" line="159"/>
+        <source>Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="155"/>
+        <source>Please choose a serial port.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="159"/>
+        <source>Failed to open!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="163"/>
+        <source>Close</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="185"/>
+        <location filename="gamepad.cpp" line="229"/>
+        <location filename="gamepad.cpp" line="251"/>
+        <location filename="gamepad.cpp" line="286"/>
+        <location filename="gamepad.cpp" line="299"/>
+        <location filename="gamepad.cpp" line="311"/>
+        <source>Warning</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="186"/>
+        <location filename="gamepad.cpp" line="230"/>
+        <location filename="gamepad.cpp" line="252"/>
+        <location filename="gamepad.cpp" line="287"/>
+        <location filename="gamepad.cpp" line="312"/>
+        <source>Do you want to save the changes you made to &quot;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="186"/>
+        <location filename="gamepad.cpp" line="230"/>
+        <location filename="gamepad.cpp" line="252"/>
+        <location filename="gamepad.cpp" line="287"/>
+        <location filename="gamepad.cpp" line="300"/>
+        <location filename="gamepad.cpp" line="312"/>
+        <source>&quot;?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="188"/>
+        <location filename="gamepad.cpp" line="232"/>
+        <location filename="gamepad.cpp" line="254"/>
+        <location filename="gamepad.cpp" line="289"/>
+        <location filename="gamepad.cpp" line="314"/>
+        <source>Don&apos;t Save</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="gamepad.cpp" line="300"/>
+        <source>Do you want to delete &quot;</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ScriptEngineEvaluation</name>
+    <message>
+        <location filename="scriptengineevaluation.cpp" line="28"/>
+        <source>Uncaught exception at line </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="scriptengineevaluation.cpp" line="28"/>
+        <source>: </source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
